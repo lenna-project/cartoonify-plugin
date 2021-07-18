9407760fb1dd269405e149fc766d087729d2249c
Christian M <chriamue@gmail.com>
2021-07-18 07:00:40 +0200

    :snake: :package: build wheel for pip

--- build.rs
+++ build.rs
@@ -10,6 +10,7 @@ fn main() {
         .display()
         .to_string();
 
+    #[cfg(not(feature = "python"))]
     cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
         .write_to_file(&output_file);
